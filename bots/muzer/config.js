eba27e4d74fbb196eb9c3583baca6379a6a4d69f
KanadeDevs <kanadebrothers1248@gmail.com>
2025-04-27 17:53:59 +0530

    feat: commands only work in specific voice channel

--- bots/muzer/config.js
+++ bots/muzer/config.js
@@ -1,18 +1,23 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    nodes: [{
-        host: "lavalink.jirayu.net",
-        password: "youshallnotpass",
-        port: 13592,
-        secure: false,
-        name: "Muzer"
-    }],
-    spotify: {
-        clientId: "a568b55af1d940aca52ea8fe02f0d93b",
-        clientSecret: "e8199f4024fe49c5b22ea9a3dd0c4789"
+  nodes: [
+    {
+      host: "lavalink.jirayu.net",
+      password: "youshallnotpass",
+      port: 13592,
+      secure: false,
     },
-    botToken: process.env.TOKEN,
-    clientId: process.env.CLIENT_ID,
-    embedColor: "#0061ff"
+  ],
+  spotify: {
+    clientId: "a568b55af1d940aca52ea8fe02f0d93b",
+    clientSecret: "e8199f4024fe49c5b22ea9a3dd0c4789",
+  },
+  botToken: process.env.TOKEN,
+  clientId: process.env.CLIENT_ID,
+  embedColor: "#0061ff",
+  botSettings: {
+    commandsChannelId: process.env.COMMANDS_CHANNEL_ID,
+    voiceChannelId: process.env.VOICE_CHANNEL_ID,
+  },
 };
