1d060fb7a2c7b4fec917fd83deb038fe4c5e8799
KanadeDevs <kanadebrothers1248@gmail.com>
2025-06-06 17:36:47 +0530

    refactor: help and ping command

---
+++ bots/astris-ai/store.js
@@ -0,0 +1,5 @@
+module.exports = {
+  headerIcon: "https://cdn.discordapp.com/emojis/1380086788990832722.gif",
+  footerIcon: "https://cdn.discordapp.com/emojis/1380506365398749315.gif",
+  embedColor: "#fb8b2e",
+};
